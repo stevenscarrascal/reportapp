fdbb243786f3e4fde97fd226501857e8bcacb598
Steven Carrascal <stevenscarrascal@hotmail.com>
2024-03-14 15:13:15 -0500

    eliminar clase dark en input

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -42,7 +43,7 @@
 
         @livewireScripts
 
-        
+
 
     </body>
 </html>
