c0541d4133ca17ba39a0001849fe72a3a7941216
Skgrendel <bryan.andion@gmail.com>
2024-03-18 17:59:44 -0500

    cambios en el form

--- resources/views/coordinador/create.blade.php
+++ resources/views/coordinador/create.blade.php
@@ -49,6 +49,16 @@
                     <div class="px-4">
                         <div class="grid gap-6 mb-6 md:grid-cols-2 px-4">
                             <div>
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de Documento</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                  <option selected>Choose a country</option>
+                                  <option value="US">United States</option>
+                                  <option value="CA">Canada</option>
+                                  <option value="FR">France</option>
+                                  <option value="DE">Germany</option>
+                                </select>
+                            </div>
+                            <div>
                                 <label for="first_name"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
                                     name</label>
