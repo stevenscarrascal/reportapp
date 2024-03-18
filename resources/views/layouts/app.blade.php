37b2ed3ae0797382fa3e87ff505ec01128730a77
Skgrendel <bryan.andion@gmail.com>
2024-03-18 07:48:09 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link rel="stylesheet" href="{{ asset('plugins/DataTables/datatables.min.css') }}">
+
     <!-- Scripts -->
     @notifyCss
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="{{ asset('plugins/DataTables/datatables.min.css') }}">
     <!-- Styles -->
     @livewireStyles
 </head>
@@ -38,8 +41,10 @@
         </main>
     </div>
     <x-notify::notify />
+ <!-- DataTables JS  y jquery 3.7 -->
     <script type="text/javascript" src="{{ asset('js/jquery-3.7.1.js') }}"></script>
     <script type="text/javascript" src="{{ asset('plugins/DataTables/datatables.min.js') }}"></script>
+
     @notifyJs
     @stack('modals')
     @include('notify::components.notify')
