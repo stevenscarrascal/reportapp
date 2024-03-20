1c24ca5d8935048fb5096d6ce3c8ec59d06c24ff
Skgrendel <bryan.andion@gmail.com>
2024-03-20 17:36:57 -0500

    integracion chartsjs

--- resources/views/informes/index.blade.php
+++ resources/views/informes/index.blade.php
@@ -1,10 +1,22 @@
 <x-app-layout>
-<x-slot name="header">
-    <div class="flex justify-between items-center">
-    <h2 class="font-semibold text-gray-800 leading-tight">
-        {{ __('Informes') }}
-    </h2>
-    </div>
-</x-slot>
+    <x-slot name="header">
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-gray-800 leading-tight">
+                {{ __('Informes') }}
+            </h2>
+        </div>
+    </x-slot>
 
+    <div class="py-12">
+        <div class=" mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+                    <div
+                        class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
+                        <canvas id="myChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
