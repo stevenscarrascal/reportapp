e13c116f8b0680558e789584c500fc95159872cf
Skgrendel <bryan.andion@gmail.com>
2024-04-05 08:03:17 -0500

    Corregir estilos y mejorar gráficos en informeGeneral.blade.php

--- resources/views/informes/informeGeneral.blade.php
+++ resources/views/informes/informeGeneral.blade.php
@@ -127,34 +127,43 @@
             });
     </script>
     <script>
-       fetch('anomaliasMes')
-    .then(response => response.json())
-    .then(data => {
-        const ctx = document.getElementById('anomaliasXmes').getContext('2d');
-        // Extraemos los nombres y los conteos de los datos
-        const labels = data.data.map(item => item.nombre);
-        const conteo = data.data.map(item => item.count);
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Anomalías',
-                    data: conteo,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
+        fetch('anomaliasMes')
+            .then(response => response.json())
+            .then(data => {
+                var colors = [
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ];
+                const ctx = document.getElementById('anomaliasXmes').getContext('2d');
+                // Extraemos los nombres y los conteos de los datos
+                const labels = data.data.map(item => item.nombre);
+                const conteo = data.data.map(item => item.count);
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Anomalías',
+                            data: conteo,
+                            backgroundColor: colors,
+                            borderColor: colors,
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
                     }
-                }
-            }
-        });
-    });
+                });
+            });
     </script>
     <script>
         fetch('ConteoPersonal')
