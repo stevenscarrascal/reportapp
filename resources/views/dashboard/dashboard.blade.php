734f698406d0cf64fccb272752dfd76107c80998
Skgrendel <bryan.andion@gmail.com>
2024-04-08 11:27:42 -0500

    Corregir URL de API de Google Maps en dashboard.blade.php

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -15,7 +15,7 @@
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"rel="stylesheet">
-        <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzpY7jFyrfHMUwJVfeXs2aihuYlAcAtsk">
+        <script async src="https://maps.googleapis.com/maps/api/js?key={{env('APP_MAPS')}}">
     </script>
     <!-- Custom styles for this template-->
 
