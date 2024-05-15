a79ea802480c8cd8ea657249c66b24b012582a62
Skgrendel <bryan.andion@gmail.com>
2024-05-15 17:11:54 -0500

    Actualizar iframe en informeGeneral.blade.php para reflejar los cambios en el enlace de Power BI

--- resources/views/informes/informeGeneral.blade.php
+++ resources/views/informes/informeGeneral.blade.php
@@ -5,7 +5,7 @@
     <div class="widget widget-chart-three">
         <div class="widget-heading">
             <div class="widget-content">
-                <iframe title="qAnalitycreporte" width="100%" height="836" src="https://app.powerbi.com/view?r=eyJrIjoiMzY2NjhlNDAtNmQyZC00NDdmLWE4MzItZjFhMmM1Y2E1ZDVjIiwidCI6ImE3MDViNGI5LWE3Y2UtNDA3YS04YTdlLTY0NThlYjVkZDQxNiJ9" frameborder="0" allowFullScreen="true"></iframe>
+                <iframe title="qAnalitycreporte" width="100%" height="836" src="https://app.powerbi.com/view?r=eyJrIjoiNTAxYTM4ZDItOGZmOS00N2Q0LTkzNTMtMjI3MDUzYjE4MTBlIiwidCI6ImE3MDViNGI5LWE3Y2UtNDA3YS04YTdlLTY0NThlYjVkZDQxNiJ9" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     </div>
