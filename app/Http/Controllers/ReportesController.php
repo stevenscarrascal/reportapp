3cada5bd9aa8df56503be44da945f4ca7400d436
Skgrendel <bryan.andion@gmail.com>
2024-05-17 12:42:54 -0500

    cambios

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\direcciones;
-use App\Models\encabezados_dets;
 use App\Models\reportes;
 use App\Models\vs_anomalias;
 use App\Models\vs_estado;
@@ -12,6 +11,7 @@ use App\Models\vs_imposibilidad;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+
 
 class ReportesController extends Controller
 {
@@ -52,80 +52,80 @@ class ReportesController extends Controller
     {
 
         $request->validate(reportes::$rules);
-
         $latitud = $request->input('latitud');
         $longitud = $request->input('longitud');
         $fontSize = 50;
 
-        if ($request->input('contrato')) {
-           $contrato = direcciones::where('contrato', $request->input('contrato'))->first();
-            if (!$contrato) {
-                notify()->error('el "Contrato" No esta en la Lista de Contratos');
+
+            if ($request->input('contrato')) {
+                $contrato = direcciones::where('contrato', $request->input('contrato'))->first();
+                if (!$contrato) {
+                    notify()->error('el "Contrato" No esta en la Lista de Contratos');
+                    return redirect()->route('reportes.create');
+                }
+            } elseif ($request->input('contrato')) {
+                $contrato = reportes::where('contrato', $request->input('contrato'))->first();
+                if ($contrato) {
+                    notify()->error('el Contrato ya fue registrado');
+                    return redirect()->route('reportes.create');
+                }
+            }
+
+            if ($latitud == null || $longitud == null) {
+                notify()->error('No se pudo obtener la direccion , Active su GPS y vuelva a intentarlo');
                 return redirect()->route('reportes.create');
+            } else {
+                $response = Http::withoutVerifying()->get("https://revgeocode.search.hereapi.com/v1/revgeocode?apikey=auuOOORgqWd_T4DFf0onY2JlvMDhz4tP0G0o7fRYDRU&at=$latitud,$longitud&lang=es-ES");
+                $data = $response->json();
+                $direccion = $data['items'][0]['address']['label'];
             }
-        }elseif ($request->input('contrato')) {
-            $contrato = reportes::where('contrato', $request->input('contrato'))->first();
-            if ($contrato) {
-                notify()->error('el Contrato ya fue registrado');
-                return redirect()->route('reportes.create');
+
+
+            $AnomaliaJson = json_encode($request->anomalia);
+            $reportes = $request->all();
+            $reportes['personal_id'] = Auth::user()->personal->id;
+            $reportes['anomalia'] = $AnomaliaJson;
+            $reportes['latitud'] = $latitud;
+            $reportes['longitud'] = $longitud;
+            $reportes['direccion'] = $direccion;
+
+            foreach (range(1, 6) as $i) {
+                if ($imagen = $request->file('foto' . $i)) {
+                    $path = 'imagen/';
+                    $foto = rand(1000, 9999) . "_" . date('YmdHis') . "." . $imagen->getClientOriginalExtension();
+                    $imagen->move($path, $foto);
+                    $reportes['foto' . $i] = $foto;
+                    //  Abrir la imagen utilizando GD
+                    $imagenGD = imagecreatefromjpeg(public_path($path . $foto));
+                    // Añadir texto del contrato  a la imagen
+                    $textoContrato = "Contrato N°:" . $request->input('contrato');
+                    $colorTexto = imagecolorallocate($imagenGD, 255, 255, 255); // Color blanco
+                    $posXContrato = 10; // Ajusta según tu diseño
+                    $posYContrato = imagesy($imagenGD) - 170; // Ajusta según tu diseño
+                    imagettftext($imagenGD, $fontSize, 0, $posXContrato, $posYContrato, $colorTexto, public_path('font/arial.ttf'), $textoContrato);
+                    // Añadir texto de coordenadas a la imagen
+                    $textoCoordenadas = "Direccion: " . $direccion;
+                    $colorTexto = imagecolorallocate($imagenGD, 255, 255, 255); // Color blanco
+                    $posXCoordenadas = 10; // Ajusta según tu diseño
+                    $posYCoordenadas = imagesy($imagenGD) - 20; // Ajusta según tu diseño
+                    imagettftext($imagenGD, $fontSize, 0, $posXCoordenadas, $posYCoordenadas, $colorTexto, public_path('font/arial.ttf'), $textoCoordenadas);
+
+                    //Añadir texto de fecha a la imagen
+                    $fechaActual = date("Y-m-d H:i:s");
+                    $posXFecha = 10; // Ajusta según tu diseño
+                    $posYFecha = imagesy($imagenGD) - 90; // Ajusta según tu diseño
+                    imagettftext($imagenGD, $fontSize, 0, $posXFecha, $posYFecha, $colorTexto, public_path('font/arial.ttf'), "Fecha: $fechaActual");
+
+                    // Guardar la imagen modificada
+                    imagejpeg($imagenGD, public_path($path . $foto));
+
+                    // Liberar la memoria
+                    imagedestroy($imagenGD);
+                }
             }
-        }
-
-        if ($latitud == null || $longitud == null) {
-            notify()->error('No se pudo obtener la direccion , Active su GPS y vuelva a intentarlo');
-            return redirect()->route('reportes.create');
-        } else {
-            $response = Http::withoutVerifying()->get("https://revgeocode.search.hereapi.com/v1/revgeocode?apikey=auuOOORgqWd_T4DFf0onY2JlvMDhz4tP0G0o7fRYDRU&at=$latitud,$longitud&lang=es-ES");
-            $data = $response->json();
-            $direccion = $data['items'][0]['address']['label'];
-        }
-
-
-        $AnomaliaJson = json_encode($request->anomalia);
-        $reportes = $request->all();
-        $reportes['personal_id'] = Auth::user()->personal->id;
-        $reportes['anomalia'] = $AnomaliaJson;
-        $reportes['latitud'] = $latitud;
-        $reportes['longitud'] = $longitud;
-        $reportes['direccion'] = $direccion;
-
-        foreach (range(1, 6) as $i) {
-            if ($imagen = $request->file('foto' . $i)) {
-                $path = 'imagen/';
-                $foto = rand(1000, 9999) . "_" . date('YmdHis') . "." . $imagen->getClientOriginalExtension();
-                $imagen->move($path, $foto);
-                $reportes['foto' . $i] = $foto;
-                //  Abrir la imagen utilizando GD
-                $imagenGD = imagecreatefromjpeg(public_path($path . $foto));
-                // Añadir texto del contrato  a la imagen
-                $textoContrato = "Contrato N°:" . $request->input('contrato');
-                $colorTexto = imagecolorallocate($imagenGD, 255, 255, 255); // Color blanco
-                $posXContrato = 10; // Ajusta según tu diseño
-                $posYContrato = imagesy($imagenGD) - 170; // Ajusta según tu diseño
-                imagettftext($imagenGD, $fontSize, 0, $posXContrato, $posYContrato, $colorTexto, public_path('font/arial.ttf'), $textoContrato);
-                // Añadir texto de coordenadas a la imagen
-                $textoCoordenadas = "Direccion: " . $direccion;
-                $colorTexto = imagecolorallocate($imagenGD, 255, 255, 255); // Color blanco
-                $posXCoordenadas = 10; // Ajusta según tu diseño
-                $posYCoordenadas = imagesy($imagenGD) - 20; // Ajusta según tu diseño
-                imagettftext($imagenGD, $fontSize, 0, $posXCoordenadas, $posYCoordenadas, $colorTexto, public_path('font/arial.ttf'), $textoCoordenadas);
-
-                //Añadir texto de fecha a la imagen
-                $fechaActual = date("Y-m-d H:i:s");
-                $posXFecha = 10; // Ajusta según tu diseño
-                $posYFecha = imagesy($imagenGD) - 90; // Ajusta según tu diseño
-                imagettftext($imagenGD, $fontSize, 0, $posXFecha, $posYFecha, $colorTexto, public_path('font/arial.ttf'), "Fecha: $fechaActual");
-
-                // Guardar la imagen modificada
-                imagejpeg($imagenGD, public_path($path . $foto));
-
-                // Liberar la memoria
-                imagedestroy($imagenGD);
-            }
-        }
-        reportes::create($reportes);
-        notify()->success('Lectura Guardada Con Exito');
-        return redirect()->route('reportes.index');
+            reportes::create($reportes);
+            notify()->success('Lectura Guardada Con Exito');
+            return redirect()->route('reportes.index');
     }
     /**
      * Display the specified resource.
@@ -220,6 +220,4 @@ class ReportesController extends Controller
     {
         //
     }
-
-
 }
